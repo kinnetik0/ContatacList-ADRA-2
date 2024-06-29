dcdc759702cc1fea53d75032f4c4dae258e5b3f8
kinnetik0 <alexis.rosquete@gmail.com>
2024-06-29 19:49:36 +0200

    Delete routes in layaout

--- src/js/layout.js
+++ src/js/layout.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop.jsx";
 import { ContactList } from "./views/contactList";
 import { AddContact } from "./views/AddContact";
-import { Single } from "./views/Single";
 import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar.jsx";
 import { Footer } from "./component/footer.jsx";
@@ -19,12 +18,6 @@ const Layout = () => {
                     <Routes>
                         <Route exact path="/" element={<ContactList />} />
                         <Route path="/add-contact" element={<AddContact />} />
-                        <Route
-                            exact
-                            path="/single/:theid"
-                            element={<Single />}
-                        />
-                        <Route exact path="*" element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
